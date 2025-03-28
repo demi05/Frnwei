b1340e7c26720abd278de51436ceee40ab597b69
Leshi Taiwo <98643006+demi05@users.noreply.github.com>
2025-03-28 12:47:15 +0100

    corrected overlay height

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -8,11 +8,9 @@ const Home = () => {
         autoPlay
         loop muted src={vid}
       >
-        {/* <source src={vid} type="video/mp4" /> */}
-        {/* Your browser does not support the video tag. */}
       </video>
 
-      <div className="absolute inset-0 bg-black opacity-20"></div>
+      <div className="absolute inset-0 bg-black opacity-20 h-screen"></div>
 
 <div className="relative z-10 flex w-50% sm:w-[50%] md:w-[45%] lg:w-[35%] xl:w-[27%] font-medium gap-12 text-[13px]">
       <div>
