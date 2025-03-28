4d1d69e03ee326127b7617686433e7905e09f534
Leshi Taiwo <98643006+demi05@users.noreply.github.com>
2025-03-28 01:46:02 +0100

    corrected text width

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ import vid from "../assets/HomeVid.mp4";
 
 const Home = () => {
   return (
-    <div className="relative w-full h-screen flex flex-col items-end text-white p-5">
+    <div className="relative w-full min-h-screen flex flex-col items-end text-white p-5">
       <video
         className="absolute inset-0 w-full h-full object-cover z-0"
         autoPlay
@@ -15,7 +15,7 @@ const Home = () => {
 
       <div className="absolute inset-0 bg-black opacity-20"></div>
 
-<div className="relative z-10 flex w-50% sm:w-[50%] md:w-[45%] lg:w-[35%] xl:w-[27%] font-semibold gap-12">
+<div className="relative z-10 flex w-50% sm:w-[50%] md:w-[45%] lg:w-[35%] xl:w-[27%] font-medium gap-12 text-[13px]">
       <div>
         <p>FRNWEI.CO IS</p>
         <p>COMING SOON</p>
